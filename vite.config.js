f85eb7a4cde641e6a9299daad00f8fba30c771b6
Aaron Dennis <aaron.dennis@Aarons-MacBook-Air.local>
2025-07-16 13:53:58 -0400

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/CryptoOracle-Live/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: 3000
-  },
-  build: {
-    outDir: 'dist'
   }
 })
 
