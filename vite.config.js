21044498bb0b1b998d1c89c807e3f429302b7cd6
Aaron Dennis <aaron.dennis@Aarons-MacBook-Air.local>
2025-07-16 13:26:16 -0400

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CryptoOracle-Frontend/', // Set to your repo name for GitHub Pages
+  base: '/CryptoOracle-Live/', // Set to your repo name for GitHub Pages
   plugins: [react()],
 })
 
