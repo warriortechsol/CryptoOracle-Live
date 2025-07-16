2ec4ad8a06b436f12e48bf17eceead487d53314e
Aaron Dennis <aaron.dennis@Aarons-MacBook-Air.local>
2025-07-16 14:43:52 -0400

    Successful build: inject bundled app into dist/index.html

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,11 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, 'index.html') // This tells Vite to start from here
+      input: resolve(__dirname, 'index.html')
     }
   },
   server: {
     port: 3000
   }
 })
-
 
